825cd9f742b6202b08e8d8e8185ad9761cf802ae
Dmytro Chushko <dmytro.chushko@gmail.com>
2022-07-11 01:03:26 +0300

    add 4 of 6 steps

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,56 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+
+import ContactForm from './ContactForm';
+
+export default class App extends Component {
+  state = {
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
+  };
+
+  handleChange = e => {
+    this.setState({ [e.currentTarget.name]: e.currentTarget.value });
+  };
+
+  handleSubmit = (id, name, number) => {
+    this.setState(({ contacts }) => ({
+      contacts: [{ id, name, number }, ...contacts],
+    }));
+  };
+
+  getVisibleContacts() {
+    return this.state.contacts.filter(contact =>
+      contact.name.includes(this.state.filter)
+    );
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Phonebook</h1>
+        <ContactForm onSubmit={this.handleSubmit} />
+        <label htmlFor={this.inputFilterId}>Find contacts by name</label>
+        <input
+          id={this.inputFilterId}
+          type="text"
+          value={this.state.filter}
+          name="filter"
+          onChange={this.handleChange}
+        />
+        <h2>Contacts</h2>
+        <ul>
+          {this.getVisibleContacts().map(({ id, name, number }) => (
+            <li key={id}>
+              {name}: {number}
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
---
+++ src/components/ContactForm/ContactForm.jsx
@@ -0,0 +1,64 @@
+import { nanoid } from 'nanoid';
+import { Component } from 'react';
+
+export default class ContactForm extends Component {
+  state = {
+    name: '',
+    number: '',
+  };
+
+  inputNameId = nanoid();
+  inputNumberId = nanoid();
+  inputFilterId = nanoid();
+
+  reset() {
+    this.setState({
+      name: '',
+      number: '',
+    });
+  }
+
+  handleChange = e => {
+    this.setState({ [e.currentTarget.name]: e.currentTarget.value });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    const id = nanoid();
+    const name = this.state.name;
+    const number = this.state.number;
+
+    this.props.onSubmit(id, name, number);
+    this.reset();
+  };
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label htmlFor={this.inputId}>Name</label>
+        <input
+          id={this.inputId}
+          type="text"
+          value={this.state.name}
+          name="name"
+          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+          required
+          onChange={this.handleChange}
+        />
+        <label htmlFor={this.inputNumberId}>Number</label>
+        <input
+          id={this.inputNumberId}
+          type="tel"
+          value={this.state.number}
+          name="number"
+          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+          title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+          required
+          onChange={this.handleChange}
+        />
+        <button type="submit">Add contact</button>
+      </form>
+    );
+  }
+}
